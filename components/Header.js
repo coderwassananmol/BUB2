9e3e42af96fa1314ad4168fceaf48e75ee55b76c
adityabisoi <adityabisoi1999@gmail.com>
2020-03-05 10:55:16 +0530

    counts from API added

--- components/Header.js
+++ components/Header.js
@@ -1,73 +1,93 @@
+import React, {Component} from 'react'
 import Head from 'next/head';
 import Link from 'next/link';
 import Octicon, { Star, RepoForked } from '@primer/octicons-react';
 
-const Header = (props) => (
-    <div>
-        <Head>
-            <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"></meta>
-            <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-                integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-                crossOrigin="anonymous" />
+class Header extends Component{
+    constructor(){
+        super()
+        this.state = {
+            gitstats: {}
+        }
+    }
+    componentDidMount(){
+        fetch("https://api.github.com/repos/coderwassananmol/BUB2")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    gitstats: data
+                })
+            })
+    }
+    render(){
+        return(
+            <div>
+                <Head>
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"></meta>
+                    <link
+                        rel="stylesheet"
+                        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+                        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+                        crossOrigin="anonymous" />
 
-            <link
-                rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-                integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
-                crossOrigin="anonymous" />
-            <link 
-                href="https://fonts.googleapis.com/css?family=Montserrat:300|Open+Sans:300|Raleway:300|Lato:700" 
-                rel="stylesheet" />
+                    <link
+                        rel="stylesheet"
+                        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+                        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+                        crossOrigin="anonymous" />
+                    <link 
+                        href="https://fonts.googleapis.com/css?family=Montserrat:300|Open+Sans:300|Raleway:300|Lato:700" 
+                        rel="stylesheet" />
 
-        </Head>
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                        <span className="sr-only">Toggle navigation</span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
-                </div>
+                </Head>
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                        </div>
 
-                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul className="nav navbar-nav">
-                        <li className={props.page === 'index' ? 'active' : null}><Link href="/"><a>Upload<span className="sr-only">(current)</span></a></Link></li>
-                        <li className={props.page === 'queue' ? 'active' : null}><Link href="/queue"><a>Queue</a></Link></li>
-                    </ul>
-                    <ul className="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="https://github.com/coderwassananmol/BUB2/"><button type="button" className="btn btn-primary"><Octicon icon={Star} /> <b>Star</b></button></a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/coderwassananmol/BUB2/fork"><button type="button" className="btn btn-primary"><Octicon icon={RepoForked} /> <b>Fork</b></button></a>
-                        </li>                        
-                        <li className="dropdown">
-                            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Help<span className="caret"></span></a>
-                            <ul className="dropdown-menu">
-                                <li><a href="#">FAQ</a></li>
-                                <li><a href="#">About</a></li>
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <li className={this.props.page === 'index' ? 'active' : null}><Link href="/"><a>Upload<span className="sr-only">(current)</span></a></Link></li>
+                                <li className={this.props.page === 'queue' ? 'active' : null}><Link href="/queue"><a>Queue</a></Link></li>
                             </ul>
-                        </li>
-                    </ul>
+                            <ul className="nav navbar-nav navbar-right">
+                                <li>
+                                    <a href="https://github.com/coderwassananmol/BUB2/"><button type="button" className="btn btn-primary"><Octicon icon={Star} /><b> Star </b><span className="badge badge-light">{this.state.gitstats.stargazers_count}</span></button></a>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/coderwassananmol/BUB2/fork"><button type="button" className="btn btn-primary"><Octicon icon={RepoForked} /><b> Fork </b><span className="badge badge-light">{this.state.gitstats.forks_count}</span></button></a>
+                                </li>                        
+                                <li className="dropdown">
+                                    <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Help<span className="caret"></span></a>
+                                    <ul className="dropdown-menu">
+                                        <li><a href="#">FAQ</a></li>
+                                        <li><a href="#">About</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <div className="panel panel-primary">
+                    <div className="panel-heading">
+                        <p className="panel-title">Book Uploader Bot (BUB) v2.0</p>
+                    </div>
+                    <div className="panel-body">
+                    <p>A bot that helps you transfer books that belong to <b>public domain</b> to Internet Archive from libraries like Google Books, West Bengal State Library etc.</p>
+                    <p>Using IA Upload Tool, you can then transfer these books to Wikimedia Commons.</p>
+                    </div>
                 </div>
             </div>
-        </nav>
-        <div className="panel panel-primary">
-            <div className="panel-heading">
-                <p className="panel-title">Book Uploader Bot (BUB) v2.0</p>
-            </div>
-            <div className="panel-body">
-            <p>A bot that helps you transfer books that belong to <b>public domain</b> to Internet Archive from libraries like Google Books, West Bengal State Library etc.</p>
-            <p>Using IA Upload Tool, you can then transfer these books to Wikimedia Commons.</p>
-            </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 export default Header;
