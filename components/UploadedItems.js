569575256dd01707dfbff1b784848a29074b577e
BhuwanChandra <bhuwan7779@gmail.com>
2020-03-12 16:08:34 +0530

    updated

--- components/UploadedItems.js
+++ components/UploadedItems.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import Link from "next/link";
 
-const finalURL = `https://archive.org/advancedsearch.php?q=bub.wikimedia+&rows=0&output=json`;
+const url = `https://archive.org/advancedsearch.php?q=bub.wikimedia+&rows=0&output=json`;
 
 class UploadedItems extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      result: 0
+      booksUploaded: 0
     }
     this.fetchData = this.fetchData.bind(this);
   }
 
   fetchData() {
-    fetch(finalURL)
+    fetch(url)
       .then((response) => response.json())
       .then((responseJson) => {
-        const resultyt = responseJson.response.numFound;
-        this.setState({ result: resultyt });
+        const booksCount = responseJson.response.numFound;
+        this.setState({ booksUploaded: booksCount });
       })
       .catch((error) => {
         console.error(error);
@@ -43,7 +43,7 @@ class UploadedItems extends Component {
         </style>
         <div className="my-2 text-center text-primary books-uploaded">
           <Link href={" https://archive.org/details/@bub_wikimedia"}>
-            <a target="_blank">{this.state.result} books uploaded to Internet Archive using BUB2!</a>
+            <a target="_blank">{this.state.booksUploaded} books uploaded to Internet Archive using BUB2!</a>
           </Link>
         </div>
       </div>
