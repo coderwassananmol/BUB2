cd01edf2f600b1f028446b8004cac8cf76bf270b
Aman-Codes <54680709+Aman-Codes@users.noreply.github.com>
2020-03-24 11:29:55 +0530

    Update Books.js

--- components/Books.js
+++ components/Books.js
@@ -44,6 +44,10 @@ export default class Books extends React.Component {
         url =
           "http://www.panjabdigilib.org/webuser/searches/displayPageContent.jsp?ID=xxxx&page=x&CategoryID=x&Searched=xxxx";
         break;
+      
+      case "ob":
+        url = "#";
+        break;
     }
     return url;
   };
@@ -209,6 +213,7 @@ export default class Books extends React.Component {
           >
             <option value="gb">Google Books</option>
             <option value="pn">Punjab Digital Library</option>
+            <option value="ob">Open Book Publishers</option>
           </select>
           <h3>
             2. Enter the {this.state.option === "gb" ? "ID" : "URI"} ({this.showExample()}){" "}
