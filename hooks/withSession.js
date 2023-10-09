8efd47ba370e54c949621b719d9151d24d6e07fe
afanyulionel <developer.ndilionel@gmail.com>
2023-10-09 18:52:59 +0100

    update withSession Hook to run both with class and Functional component

--- hooks/withSession.js
+++ hooks/withSession.js
@@ -3,7 +3,8 @@ import { useSession } from "next-auth/react";
 export const withSession = (Component) => (props) => {
   const session = useSession();
 
-  if (Component.prototype.render) {
+  if (Component.prototype && Component.prototype?.render) {
     return <Component session={session} {...props} />;
   }
+  return null;
 };
