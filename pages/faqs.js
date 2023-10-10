f9e91d095b84fd273879daf3a7f0415e6795c739
Ed-Gah <edwingah2021@gmail.com>
2023-10-10 11:01:35 +0100

    Fixes on redesign FAQs page

---
+++ pages/faqs.js
@@ -0,0 +1,34 @@
+import Header from "../components/Header";
+import FaqsSection from "../components/FaqsSection";
+import { faq_data } from "../utils/constants";
+
+const Faqs = () => {
+  return (
+    <div>
+      <style jsx>
+        {`
+          .faq-container {
+            color: #36c;
+            flex-grow: 0;
+            overflow: auto;
+            height: 70vh;
+          }
+          .
+        `}
+      </style>
+      <Header page="faqs" />
+      <div>
+        <div className="container p-0">
+          <div className="main-content">
+            <h4>Frequently Asked Questions</h4>
+            <div className="faq-container">
+              <FaqsSection faqs_data={faq_data} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faqs;
