297536f547a579c6b14b318eb8a7540edd543f49
FindoKampete <f.kampete@batazia.com>
2023-10-11 02:11:17 +0100

    issue-T348471-BUB2: Fixed error in withSession HOC when Header.js component is converted to a functional stateless component

--- hooks/withSession.js
+++ hooks/withSession.js
@@ -3,7 +3,5 @@ import { useSession } from "next-auth/react";
 export const withSession = (Component) => (props) => {
   const session = useSession();
 
-  if (Component.prototype.render) {
-    return <Component session={session} {...props} />;
-  }
+  return <Component session={session} {...props} />;
 };
