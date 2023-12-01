6fb71b4564238f4065e679d16affc997c11de6fe
Okereke Chinweotito <okerekechinweotito@gmail.com>
2023-12-01 12:01:47 +0100

    add wikisource to sender and improve code

--- bull/email-queue/consumer.js
+++ bull/email-queue/consumer.js
@@ -10,35 +10,23 @@ Mediawiki Email API DOCS - https://www.mediawiki.org/wiki/API:Emailuser#JavaScri
 MWN TOOLFORGE PACKAGE DOCS -https://github.com/siddharthvp/mwn
 */
 
-async function mediawikiEmail(username, title, trueURI, success, done) {
-  const bot = await Mwn.init({
-    apiUrl: "https://en.wikipedia.org/w/api.php",
-    username: process.env.EMAIL_BOT_USERNAME,
-    password: process.env.EMAIL_BOT_PASSWORD,
-    // Set your user agent (required for WMF wikis, see https://meta.wikimedia.org/wiki/User-Agent_policy):
-    userAgent: "bub2.toolforge ([[https://bub2.toolforge.org]])",
-    // Set default parameters to be sent to be included in every API request
-    defaultParams: {
-      assert: "user", // ensure we're logged in
-    },
-  });
-  function getCsrfToken() {
-    return bot
-      .request({
-        action: "query",
-        meta: "tokens",
-        format: "json",
-      })
-      .then((data) => {
-        send_email(data.query.tokens.csrftoken);
-      })
-      .catch((error) => {
-        console.error("Error while getting CSRF token:", error);
-      });
-  }
+async function mediawikiEmail(username, title, trueURI, success) {
+  try {
+    const bot = await Mwn.init({
+      apiUrl: "https://wikisource.org/w/api.php",
+      username: process.env.EMAIL_BOT_USERNAME,
+      password: process.env.EMAIL_BOT_PASSWORD,
+      // Set your user agent (required for WMF wikis, see https://meta.wikimedia.org/wiki/User-Agent_policy):
+      userAgent: "bub2.toolforge ([[https://bub2.toolforge.org]])",
+      // Set default parameters to be sent to be included in every API request
+      defaultParams: {
+        assert: "user", // ensure we're logged in
+      },
+    });
 
-  function send_email(csrf_token) {
-    return bot
+    const csrf_token = bot.getCsrfToken();
+
+    bot
       .request({
         action: "emailuser",
         target: username,
@@ -55,28 +43,36 @@ async function mediawikiEmail(username, title, trueURI, success, done) {
           level: "info",
           message: `Email Sent Successfully! Result : ${data}`,
         });
-        done(null, true);
+        return 200;
       })
       .catch((error) => {
         logger.log({
           level: "error",
           message: `Failed to send email with error:  ${error}`,
         });
-        done(new Error(error));
+        return error;
       });
+  } catch (error) {
+    logger.log({
+      level: "error",
+      message: `mediawikiEmail:  ${error}`,
+    });
+    return error;
   }
-  getCsrfToken();
 }
 
 EmailQueue.process(async (job, done) => {
   if (job.data.isEmailNotification === "true") {
-    mediawikiEmail(
+    const emailResponse = mediawikiEmail(
       job.data.userName,
       job.data.title,
       job.data.trueURI,
-      job.data.success,
-      done
+      job.data.success
     );
+    if (emailResponse !== 200) {
+      done(new Error(`mediawikiEmail: ${emailResponse}`));
+    }
+    done(null, true);
   } else {
     done(null, true);
   }
